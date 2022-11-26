ee6141bf2c47b32a7ddc569db1f844979aa7b42d
doge <40008184+Moon-is-beautiful@users.noreply.github.com>
2022-11-26 00:05:32 -0600

    Add files via upload

---
+++ SpaceInvaders.c
@@ -0,0 +1,212 @@
+// SpaceInvaders.c
+// Runs on TM4C123
+// Jonathan Valvano and Daniel Valvano
+// This is a starter project for the EE319K Lab 10
+
+// Last Modified: 8/24/2022 
+// http://www.spaceinvaders.de/
+// sounds at http://www.classicgaming.cc/classics/spaceinvaders/sounds.php
+// http://www.classicgaming.cc/classics/spaceinvaders/playguide.php
+
+// ******* Possible Hardware I/O connections*******************
+// Slide pot pin 1 connected to ground
+// Slide pot pin 2 connected to PD2/AIN5
+// Slide pot pin 3 connected to +3.3V 
+// buttons connected to PE0-PE3
+// 32*R resistor DAC bit 0 on PB0 (least significant bit)
+// 16*R resistor DAC bit 1 on PB1
+// 8*R resistor DAC bit 2 on PB2 
+// 4*R resistor DAC bit 3 on PB3
+// 2*R resistor DAC bit 4 on PB4
+// 1*R resistor DAC bit 5 on PB5 (most significant bit)
+// LED on PD1
+// LED on PD0
+
+
+#include <stdint.h>
+#include "../inc/tm4c123gh6pm.h"
+#include "../inc/ST7735.h"
+#include "Random.h"
+#include "TExaS.h"
+#include "../inc/ADC.h"
+#include "Images.h"
+#include "Sound.h"
+#include "Timer1.h"
+
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+void Delay100ms(uint32_t count); // time delay in 0.1 seconds
+void hitcheck(); //function that checks if anything hit each other
+void draw();//draw anything that exist
+struct bhd{
+	int coordx,coordy;
+	const uint16_t *p;
+	int w,h,exist,health;
+};
+typedef struct bhd state;
+state enemy[12]={
+	{0,0,SmallEnemy30pointB,16,10,0,0},//e1
+	{0,0,SmallEnemy20pointB,16,10,0,0},//e2
+	{0,0,SmallEnemy10pointB,16,10,0,0},//e3
+	{0,0,SmallEnemy30pointB,16,10,0,0},//e4
+	{0,0,SmallEnemy20pointB,16,10,0,0},//e5
+	{0,0,SmallEnemy10pointB,16,10,0,0},//e6
+	{0,0,SmallEnemy30pointB,16,10,0,0},//e7
+	{0,0,SmallEnemy20pointB,16,10,0,0},//e8
+	{0,0,SmallEnemy10pointB,16,10,0,0},//e9
+	{0,0,SmallEnemy30pointB,16,10,0,0},//e10
+	{0,0,SmallEnemy20pointB,16,10,0,0},//e11
+	{0,0,SmallEnemy10pointB,16,10,0,0},//e12
+};
+state enemy_bullet[3]={ 
+	{0,0,SmallEnemy10pointB,16,10,0,0},
+	{0,0,SmallEnemy10pointB,16,10,0,0},
+	{0,0,SmallEnemy10pointB,16,10,0,0}
+};
+state player[3]={ 
+	{0,0,PlayerShip0,16,10,1,3}
+};
+state player_bullet[1]={ 
+	{0,0,SmallEnemy10pointB,16,10,0,0}
+};
+state bunker[3]={
+	{0,0,Bunker0,16,10,0,0},
+	{0,0,Bunker0,16,10,0,0},
+	{0,0,Bunker0,16,10,0,0}
+};
+
+
+void Timer1A_Handler(void){ // can be used to perform tasks in background
+  TIMER1_ICR_R = TIMER_ICR_TATOCINT;// acknowledge TIMER1A timeout
+   // execute user task
+}
+
+int main1(void){
+  DisableInterrupts();
+  TExaS_Init(NONE);       // Bus clock is 80 MHz 
+  Random_Init(1);
+
+  Output_Init();
+  ST7735_FillScreen(0x0000);            // set screen to black
+  
+  ST7735_DrawBitmap(22, 159, PlayerShip0, 18,8); // player ship bottom
+  ST7735_DrawBitmap(53, 151, Bunker0, 18,5);
+  ST7735_DrawBitmap(42, 159, PlayerShip1, 18,8); // player ship bottom
+  ST7735_DrawBitmap(62, 159, PlayerShip2, 18,8); // player ship bottom
+  ST7735_DrawBitmap(82, 159, PlayerShip3, 18,8); // player ship bottom
+
+  ST7735_DrawBitmap(0, 9, SmallEnemy10pointA, 16,10);
+  ST7735_DrawBitmap(20,9, SmallEnemy10pointB, 16,10);
+  ST7735_DrawBitmap(40, 9, SmallEnemy20pointA, 16,10);
+  ST7735_DrawBitmap(60, 9, SmallEnemy20pointB, 16,10);
+  ST7735_DrawBitmap(80, 9, SmallEnemy30pointA, 16,10);
+  ST7735_DrawBitmap(100, 9, SmallEnemy30pointB, 16,10);
+
+  Delay100ms(50);              // delay 5 sec at 80 MHz
+
+  ST7735_FillScreen(0x0000);   // set screen to black
+  ST7735_SetCursor(1, 1);
+  ST7735_OutString("GAME OVER");
+  ST7735_SetCursor(1, 2);
+  ST7735_OutString("Nice try,");
+  ST7735_SetCursor(1, 3);
+  ST7735_OutString("Earthling!");
+  ST7735_SetCursor(2, 4);
+  ST7735_OutUDec(1234);
+  while(1){
+  }
+
+}
+
+
+// You can't use this timer, it is here for starter code only 
+// you must use interrupts to perform delays
+void Delay100ms(uint32_t count){uint32_t volatile time;
+  while(count>0){
+    time = 727240;  // 0.1sec at 80 MHz
+    while(time){
+      time--;
+    }
+    count--;
+  }
+}
+typedef enum {English, Spanish, Portuguese, French} Language_t;
+Language_t myLanguage=English;
+typedef enum {HELLO, GOODBYE, LANGUAGE} phrase_t;
+const char Hello_English[] ="Hello";
+const char Hello_Spanish[] ="\xADHola!";
+const char Hello_Portuguese[] = "Ol\xA0";
+const char Hello_French[] ="All\x83";
+const char Goodbye_English[]="Goodbye";
+const char Goodbye_Spanish[]="Adi\xA2s";
+const char Goodbye_Portuguese[] = "Tchau";
+const char Goodbye_French[] = "Au revoir";
+const char Language_English[]="English";
+const char Language_Spanish[]="Espa\xA4ol";
+const char Language_Portuguese[]="Portugu\x88s";
+const char Language_French[]="Fran\x87" "ais";
+const char *Phrases[3][4]={
+  {Hello_English,Hello_Spanish,Hello_Portuguese,Hello_French},
+  {Goodbye_English,Goodbye_Spanish,Goodbye_Portuguese,Goodbye_French},
+  {Language_English,Language_Spanish,Language_Portuguese,Language_French}
+};
+
+int main(void){ char l;
+  DisableInterrupts();
+  TExaS_Init(NONE);       // Bus clock is 80 MHz 
+  Output_Init();
+  ST7735_FillScreen(0x0000);            // set screen to black
+  for(phrase_t myPhrase=HELLO; myPhrase<= GOODBYE; myPhrase++){
+    for(Language_t myL=English; myL<= French; myL++){
+         ST7735_OutString((char *)Phrases[LANGUAGE][myL]);
+      ST7735_OutChar(' ');
+         ST7735_OutString((char *)Phrases[myPhrase][myL]);
+      ST7735_OutChar(13);
+    }
+  }
+  Delay100ms(30);
+  ST7735_FillScreen(0x0000);       // set screen to black
+  l = 128;
+  while(1){
+    Delay100ms(20);
+    for(int j=0; j < 3; j++){
+      for(int i=0;i<16;i++){
+        ST7735_SetCursor(7*j+0,i);
+        ST7735_OutUDec(l);
+        ST7735_OutChar(' ');
+        ST7735_OutChar(' ');
+        ST7735_SetCursor(7*j+4,i);
+        ST7735_OutChar(l);
+        l++;
+      }
+    }
+  }  
+}
+
+void draw(){
+	int i;
+	for(i=0;i<=12;i++){
+		if(enemy[i].exist==1){
+			ST7735_DrawBitmap(enemy[i].coordx,enemy[i].coordy,enemy[i].p,enemy[i].w,enemy[i].h);
+		}
+	}
+	for(i=0;i<=3;i++){
+		if(enemy_bullet[i].exist==1){
+			ST7735_DrawBitmap(enemy_bullet[i].coordx,enemy_bullet[i].coordy,enemy_bullet[i].p,enemy_bullet[i].w,enemy_bullet[i].h);
+		}
+	}
+	for(i=0;i<=3;i++){
+		if(bunker[i].exist==1){
+			ST7735_DrawBitmap(bunker[i].coordx,bunker[i].coordy,bunker[i].p,bunker[i].w,bunker[i].h);
+		}
+	}
+		if(player[0].exist==1){
+			ST7735_DrawBitmap(player[i].coordx,player[i].coordy,player[i].p,player[i].w,player[i].h);
+		}
+		if(player_bullet[0].exist==1){
+			ST7735_DrawBitmap(player_bullet[i].coordx,player_bullet[i].coordy,player_bullet[i].p,player_bullet[i].w,player_bullet[i].h);
+		}
+}
+void hitcheck(){
+	
+}
